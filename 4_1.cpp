be270499181643f6c52561e552f1e9e12b08d908
zhangheng12 <31738128+zhangheng12@users.noreply.github.com>
2017-10-16 00:19:04 +0800

    Add files via upload

---
+++ 4_1.cpp
@@ -0,0 +1,13 @@
+#include<iostream.h>
+void main()
+{
+ int a,i=1,isum=0;
+ while(a>=2000&&a<=3000,a%4==0)
+ {
+  isum=isum+i;
+  cout<<"a="<<a<<",isum="<<isum<<endl;
+ }
+ cout<<"isum="<<isum<<endl;
+ cout<<"a="<<a<<endl;
+}
+  
