964265a92fcbac4c83a913b58b42b00ff7c5e075
zhangheng12 <31738128+zhangheng12@users.noreply.github.com>
2017-10-16 00:14:53 +0800

    Add files via upload

---
+++ 3_3.cpp
@@ -0,0 +1,17 @@
+#include<iostream.h>
+void main()
+{
+ float r,s,l,pi;
+ int k;
+ cin>>k;
+ pi=3.14159;
+ s=pi*r*r;
+ l=2*pi*r;
+ switch(k)
+ {
+  case 1:cout<<"s="<<s<<endl;
+  case 2:cout<<"l="<<l<<endl;
+  case 3:cout<<"s="<<s<<"l="<<endl;
+ }
+}
+
