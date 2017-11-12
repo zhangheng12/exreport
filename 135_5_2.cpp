e95229b62a2db1fb02eb12320538b7164cb4762f
zhangheng12 <31738128+zhangheng12@users.noreply.github.com>
2017-11-13 00:43:59 +0800

    Add files via upload

---
+++ 135_5_2.cpp
@@ -0,0 +1,21 @@
+#include"iostream.h"
+void james(int a)
+{
+	int n,a=1;
+	float isum=0;
+	while(a<=n)
+	{
+		isum=isum+1/a;
+		return(isum);
+		a++;
+	}
+}
+	void main()
+	{
+		james(int a);
+		float S,isum;
+		S=isum;
+		cout<<"S="<<"1+1/(1+2)+1/(1+2+3)+бнбн+1/(1+2+3бнбн+n)="<<S<<endl;
+	}
+	
+		
